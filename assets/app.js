068a4a28de2e79d22d2be4fa1e1f96b98bcb1298
Anett David <davidanett@gmail.com>
2021-11-07 18:51:22 +0100

    Install and test react

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,7 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
+import React from 'react';
+import ReactDom from 'react-dom';
 
-// start the Stimulus application
-import './bootstrap';
+const el = React.createElement('h2', null, 'Coucou React');
 
-
+ReactDom.render(el, document.getElementById('tasklist-manager-app'));
